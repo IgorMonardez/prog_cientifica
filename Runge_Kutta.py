fb98502b327fd081cf0d339dba513f4fcec1feff
IMonardez <igormonardez@id.uff.br>
2024-10-30 17:43:57 -0300

    Reformatação dos prints

--- Runge_Kutta.py
+++ Runge_Kutta.py
@@ -5,6 +5,8 @@ def runge_kutta(expressao, w, t_inicial, t_final, divisao):
     h = (t_final - t_inicial) / divisao
     i = 1
     while t_inicial <= t_final:
+        print("t: " + str(t_inicial) + "\n")
+
         k1 = h * funcao(expressao, {'t': t_inicial, 'w': w})
         k2 = h * funcao(expressao, {'t': t_inicial + h / 2, 'w': w + k1 / 2})
         k3 = h * funcao(expressao, {'t': t_inicial + h / 2, 'w': w + k2 / 2})
@@ -14,9 +16,9 @@ def runge_kutta(expressao, w, t_inicial, t_final, divisao):
         print(f"k2: {k2}")
         print(f"k3: {k3}")
         print(f"k4: {k4}")
-        print(f"w{i}: {w}")
+        print(f"w{i}: {w}\n\n")
         i += 1
         t_inicial += h
 
 
-print(runge_kutta('1 + (t-w)**2', 1, 2, 3, 2))
+runge_kutta('1 + (t-w)**2', 1, 2, 3, 2)
