6c06a803fa7a83b1fbdb6abd2cb18aaa7cc79b7a
IMonardez <igormonardez@id.uff.br>
2024-10-31 09:47:56 -0300

    Refatoração para aceitar o número de euler

--- metodos/Runge_Kutta.py
+++ metodos/Runge_Kutta.py
@@ -1,15 +1,15 @@
 from core.Funcao import funcao
-
+import math
 
 def runge_kutta_4_ordem(expressao, w, t_inicial, t_final, divisao):
     h = (t_final - t_inicial) / divisao
     i = 1
     while t_inicial <= t_final:
         print(f"Iteração {i}")
-        k1 = h * funcao(expressao, {'t': t_inicial, 'w': w})
-        k2 = h * funcao(expressao, {'t': t_inicial + h / 2, 'w': w + k1 / 2})
-        k3 = h * funcao(expressao, {'t': t_inicial + h / 2, 'w': w + k2 / 2})
-        k4 = h * funcao(expressao, {'t': t_inicial + h, 'w': w + k3})
+        k1 = h * funcao(expressao, {'t': t_inicial, 'w': w, 'e': math.e})
+        k2 = h * funcao(expressao, {'t': t_inicial + h / 2, 'w': w + k1 / 2, 'e': math.e})
+        k3 = h * funcao(expressao, {'t': t_inicial + h / 2, 'w': w + k2 / 2, 'e': math.e})
+        k4 = h * funcao(expressao, {'t': t_inicial + h, 'w': w + k3, 'e': math.e})
         w = w + (k1 + 2 * k2 + 2 * k3 + k4) / 6
         print(f"k1: {k1}")
         print(f"k2: {k2}")
